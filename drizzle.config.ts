7e2c24562d219d89ee2233d09f553038aebf55e6
eko9x9 <ekonurrohman969@gmail.com>
2024-05-01 11:15:00 +0700

    init commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,6 @@ dotenv.config({
     path: ".env.local"
 });
 
-// We need to make sure the in the tsconfig.json file, we need to change the target at least to 'ES6'
 export default {
     schema: './src/db/schema/index.ts',
     out: './outs/drizzle',
